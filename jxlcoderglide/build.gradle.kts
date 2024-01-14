43432c2c1b73df218242f2d7052a2ac97b354585
awxkee <qnljoy@gmail.com>
2024-01-14 17:49:31 +0000

    Update to 1.8.0

--- jxlcoderglide/build.gradle.kts
+++ jxlcoderglide/build.gradle.kts
@@ -68,9 +68,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    api("io.coil-kt:coil:2.4.0")
+    api("io.coil-kt:coil:2.5.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     kapt("com.github.bumptech.glide:compiler:4.16.0")
-    api("com.github.awxkee:jxl-coder:1.7.6")
+    api("com.github.awxkee:jxl-coder:1.8.0")
 
 }
